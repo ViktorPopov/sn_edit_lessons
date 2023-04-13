a82717ed2016af9647ac1dc470945fa93a514601
ViktorPopov <welcomealiens@gmail.com>
2023-04-13 23:42:57 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sn_edit_lessons/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sn_edit_lessons
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+
+
