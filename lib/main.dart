03f2adfe5a3159920b9e6b3552e1309be69ab365
doge <jekcatpopov@gmail.com>
2023-04-16 20:28:26 +0300

    Initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sn_edit_lessons/view/Screens/HomePage.dart';
 
-void main() {
+void main() async {
+  Firebase.initializeApp();
   runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
--- lib/view/Screens/MainSceeens/SelectLanguage.dart
+++ lib/view/Screens/MainSceeens/SelectLanguage.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:sn_edit_lessons/view/Screens/MainSceeens/SelectLevel.dart';
 
 class LanguageListScreen extends StatefulWidget {
   const LanguageListScreen({Key? key}) : super(key: key);
@@ -11,12 +13,43 @@ class _LanguageListScreenState extends State<LanguageListScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView.builder(itemBuilder: (context, index) {
-        return const Card(child: Row(children: [
-          Text('Language'),
-        ],),);
-      },
-      itemCount: 10,),
+      body: StreamBuilder<QuerySnapshot>(
+          stream: FirebaseFirestore.instance
+              .collection('speak_now_lessons')
+              .snapshots(),
+          builder:
+              (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+            if (snapshot.hasError) {
+              return const Text('что то пошло не так');
+            }
+
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Material(
+                child: Center(
+                  child: CircularProgressIndicator(
+                    color: Colors.red,
+                  ),
+                ),
+              );
+            }
+
+            return SizedBox(
+              height: MediaQuery.of(context).size.height / 1.7,
+              child: ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (context, index) {
+                    var anketss = snapshot.data!.docs[index];
+                    return GestureDetector(
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => SelectLevel(launguage: '')));
+                        },
+                        child: Card(
+                          child: Text('${anketss.get('Language')}'),
+                              ),
+                        );
+                  }),
+            );
+          }),
     );
   }
 }
